54805ce4b43b5a02fd2b0efd2baf574cdc6b8c30
uzair9 <studies9990@gmail.com>
2021-05-27 02:10:22 +0500

    File Cleanup

--- static/home/static-home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-// serving template files for home
-app.use('/css', express.static('./public/home/css'));
-app.use('/fonts', express.static('./public/home/fonts'));
-app.use('/icon', express.static('./public/home/icon'));
-app.use('/img', express.static('./public/home/img'));
-app.use('/js', express.static('./public/home/js'));
